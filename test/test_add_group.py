aab89319e42136df8c0046a4f2ad8d213cfda793
Pierre <pstoiko@gmail.com>
2019-01-06 15:59:31 -0500

    Переделываем тесты: проверяем данные, загруженные из базы данных | test_add_group passed

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,22 +2,20 @@
 from model.group import Group
 
 
-def test_add_group(app, json_groups):
+def test_add_group(app, db, json_groups):
     group = json_groups
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     app.group.create_group(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
+def test_add_empty_group(app, db):
+    old_groups = db.get_group_list()
     group = Group(name="", header=" header", footer="")
     app.group.create_group(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
