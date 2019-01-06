7ccf54debd89c9dcb7801bcde2208c880ffef4c8
Pierre <pstoiko@gmail.com>
2019-01-06 17:20:42 -0500

    Переделываем тесты: проверяем данные, загруженные из базы данных | test_modify_group passed

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -15,7 +15,7 @@ def test_modify_first_group_name(app):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_modify_some_group_name(app):
+def test_modify_some_group_name(app, db):
     if app.group.count() == 0:
         app.group.create_group(Group(name='Test'))
     old_groups = app.group.get_group_list()
