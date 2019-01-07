cafb7537622265ad5cb3de9f19dbbf45af45a8cf
PStoiko <pstoiko@gmail.com>
2019-01-07 11:26:57 -0500

    Отключаемые проверки пользовательского интерфейса. test_del_group failed

--- conftest.py
+++ conftest.py
@@ -28,6 +28,12 @@ def app(request):
     return fixture
 
 
+@pytest.fixture
+def check_ui(request):
+    return request.config.getoption('--check_ui')
+
+
+
 @pytest.fixture(scope='session')
 def db(request):
     db_config = load_config(request.config.getoption('--target'))['db']
@@ -51,6 +57,8 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption('--browser', action='store', default='chrome')
     parser.addoption('--target', action='store', default='target.json')
+    parser.addoption('--check_ui', action='store_true')
+
 
 
 def pytest_generate_tests(metafunc):
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -13,7 +13,7 @@ def test_delete_first_group(app, db):
     assert old_groups == new_groups
 
 
-def test_delete_some_group(app, db):
+def test_delete_some_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create_group(Group(name='Test'))
     old_groups = db.get_group_list()
@@ -22,3 +22,5 @@ def test_delete_some_group(app, db):
     new_groups = db.get_group_list()
     old_groups.remove(group)
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
