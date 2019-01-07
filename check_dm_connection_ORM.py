500a1978b06f8b46d7c2c77ca11242292b1771cd
PStoiko <pstoiko@gmail.com>
2019-01-07 16:40:19 -0500

    ORM – Object Relational Mapping. Done

--- check_dm_connection_ORM.py
+++ check_dm_connection_ORM.py
@@ -5,7 +5,7 @@ from model.group import Group
 db = ORMFixture(host='127.0.0.1', name='addressbook', user='root', password='')
 
 try:
-    l = db.get_contacts_in_group(Group(id='270'))
+    l = db.get_contacts_not_in_group(Group(id='270'))
     for item in l:
         print(item)
     print(len(l))
